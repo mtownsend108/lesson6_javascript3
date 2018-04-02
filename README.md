d2330ae55c69d782f2883f9c4942c56bffd7f9a3
Raven Townsend <marcella.raven.townsend@gmail.com>
2018-04-02 20:17:28 +0000

    lesson 6 final commit

--- README.md
+++ README.md
@@ -4,19 +4,18 @@
 Web Application Development
 
 ### Assignment Date:  
-(Month Day, Year)
+April 2018, Spring 2018 Semester
 
 ### Student Name:  
-(First and Last Name)
+M. Raven Townsend, Web Development Student
 
 ### Project Description:
-(In your own words, briefly describe the assignment.)
+This assignment covered the introduced the Document Object Model (DOM).  JavaScript gets all of the power it needs to create dynamic HTML from the DOM.
 
 ### View Project:
-(Replace this statement with your Github Page URL that was created when you 
- published the project.)
+Github link: https://mtownsend108.github.io/lesson6_javascript3/
 
 ### Lessons Learned in the Assignment:
-1. (Briefly describe a lesson/concept learned in this lesson.)
-2. (Briefly describe a lesson/concept learned in this lesson.)
-3. (Briefly describe a lesson/concept learned in this lesson.)
+1. Access elements by id, tag name, class name, or selector
+2. Access element content, CSS properties, and attributes
+3. Add and remove document nodes and understanding the DOM and BOM.
--- photos.js
+++ photos.js
@@ -1,7 +1,7 @@
 /*    
     Program Name:  Photo Gallery Application
-    Author: 
-    Date:   
+    Author: M. Raven Townsend
+    Date:   March 2018, Spring 2018
     Filename: photos.js
     
  */
@@ -13,13 +13,29 @@
 var photoOrder = [1,2,3,4,5];
 var figureCount = 3;
 
+
 /*****************************************populateFigures() FUNCTION*************************************/
 /* This method adds src values to img elements based on order specified in photoOrder array             */
 /********************************************************************************************************/
 function populateFigures() {
+    var filename;
+    var currentFig;
+    
+    if (figureCount === 3) {
+    for (var i = 1; i < 4; i++) {
+        filename = "images/IMG_0" + photoOrder[i] + "sm.jpg";
+        currentFig = document.getElementsByTagName("img")[i-1];
+        currentFig.src = filename;
+    }//end of for
 
-
-
+        
+    }  else {
+        for (var i = 0; i < 5; i++) {
+        filename = "images/IMG_0" + photoOrder[i] + "sm.jpg";
+        currentFig = document.getElementsByTagName("img")[i];
+        currentFig.src = filename;
+        }//end of for loop
+    }//end of else
 
 }//end of populateFigures function
 
@@ -69,9 +85,46 @@ function leftArrow() {
 /******************************************************************************************************/
 function previewFive() {
    
+    //locate first element where the tag is article assign to a variable
+    var articleE1 = document.getElementsByTagName("article")[0];
+    
+    //create figure and img elements for fifth image
+    var lastFigure = document.createElement("figure");
+        
+        lastFigure.id = "fig5";
+        lastFigure.style.zIndex = "5";
+        lastFigure.style.position = "absolute";
+        lastFigure.style.right = "45px";
+        lastFigure.style.top = "67px";
+    
+    var lastImage = document.createElement("img");
+        lastImage.width = "240";
+        lastImage.height = "135";
+   
+    //add or attach the fifth image to preview
+        lastFigure.appendChild(lastImage);
 
-   
-   
+        articleE1.appendChild(lastFigure);
+    
+    //clone figure element for fifth image and edit to be first image
+    var firstFigure = lastFigure.cloneNode(true);
+    
+        firstFigure.id = "fig1";
+        firstFigure.style.right = "";
+        firstFigure.style.left = "45px";
+    
+        articleE1.insertBefore(firstFigure, document.getElementById("fig2"));
+    
+    //add appropriate src values to two new img elements
+    document.getElementsByTagName("img")[0].src = "images/IMG_0" + photoOrder[0] + "sm.jpg";
+    document.getElementsByTagName("img")[4].src = "images/IMG_0" + photoOrder[4] + "sm.jpg";
+    
+    figureCount = 5;
+
+    //disable the "show more images" button after it has been selected once
+    var numberButton = document.querySelector("#fiveButton p");
+    numberButton.removeEventListener("click", previewFive, false);
+    
 }//end of previewFive()
 
 /****************************************END of previewFive() Function**********************************/
@@ -82,10 +135,30 @@ function previewFive() {
 /*         This method attaches event listeners to the buttons (left, right, and show all).            */
 /*******************************************************************************************************/
 function createEventListeners() {
-   
 
+    //declare variable for document element with id of leftarrow in the html document
+    var leftarrow = document.getElementById("leftarrow");
 
-   
+    //add an event handler to the specified document element (leftarrow)
+    //the leftArrow() function will executed when user clicks the left navigation arrow
+    leftarrow.addEventListener("click", leftArrow, false); 
+
+    //declare variable for document element with id of rightarrow in the html document
+    var rightarrow = document.getElementById("rightarrow");
+
+    //add an event handler to the specified document element (rightarrow)
+    //the rightArrow() function will executed when user clicks the right navigation arrow 
+    rightarrow.addEventListener("click", rightArrow, false);
+    
+    //declare a variable for the element fiveButton
+    var showAllButton = document.querySelector("#fiveButton p");
+
+    //add event listener to showAllButton element
+    //when the user clicks the Show More button the previewFive function is executed
+    showAllButton.addEventListener("click", previewFive, false);
+    
+  
+    
 }//end of createEventListeners function
 
 /************************************END OF createEventListeners() FUNCTION****************************/
@@ -96,6 +169,8 @@ function createEventListeners() {
 function setUpPage() {
    createEventListeners();
    populateFigures();
+   
+   
 }//end of setUpPage Function
 
 /***********************************END OF setUpPage() FUNCTION***************************************/
